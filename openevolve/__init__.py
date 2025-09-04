37ba871b5ddebd3b342037bb09337d8988959872
Asankhaya Sharma <codelion@users.noreply.github.com>
2025-09-04 12:57:02 +0800

    fxies

--- openevolve/__init__.py
+++ openevolve/__init__.py
@@ -4,5 +4,20 @@ OpenEvolve: An open-source implementation of AlphaEvolve
 
 from openevolve._version import __version__
 from openevolve.controller import OpenEvolve
+from openevolve.api import (
+    run_evolution,
+    evolve_function,
+    evolve_algorithm,
+    evolve_code,
+    EvolutionResult
+)
 
-__all__ = ["OpenEvolve", "__version__"]
+__all__ = [
+    "OpenEvolve", 
+    "__version__",
+    "run_evolution",
+    "evolve_function", 
+    "evolve_algorithm",
+    "evolve_code",
+    "EvolutionResult"
+]
